a720c0d3028d1246323ef2dbfd8e1ef6646da40a
lyg <1983654762@qq.com>
2019-01-19 16:46:29 +0800

    default

--- food/robots.py
+++ food/robots.py
@@ -4,6 +4,11 @@ import time
 from lxml import etree
 import json
 
+import MySQLdb
+
+import sys
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
 
 # with open("robots.json","r") as f:
 #     content = f.read()
@@ -14,37 +19,68 @@ HEADERS = {'Accept': 'text/html, application/xhtml+xml, image/jxr, */*',
            'Connection':'Keep-Alive',
            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36 Edge/15.15063'}
 
-def deal_content(resp):
-    content = resp.content
+BASIC_URL = "https://weixin.sogou.com/weixin?query=%s&_sug_type_=&s_from=input&_sug_=n&type=1&page=%s&ie=utf8"
 
-    # print "="*77
-    # print content
-    # print "="*77
+conn=MySQLdb.connect(host="119.23.52.3",user="root",passwd="168mysql",db="haha",charset="utf8")  
+cursor = db.cursor()
 
-    html = etree.HTML(content)
+def search_list(word):
+    for i in range(1,11):
+        url = BASIC_URL%(word,i)
+        resp = requests.get(url,headers=HEADERS)
+        html = etree.HTML(resp.content)
+        # img_srcs = html.xpath("//li//div[@class='img-box']//img/@src")
+        detail_srcs = html.xpath("//li//div/p[@class='tit']/a/@href")
 
-    img_srcs = html.xpath("/html/body//td/a/img/@src")
-    names = html.xpath("/html/body//td[last()]/a[last()-1]/text()")
+        for src in detail_srcs:
+            resp = requests.get(src,headers=HEADERS)
+            html = etree.HTML(resp.content)
+            head = html.xpath("//div//span/img/@src")[0].replace("http","https")
+            name = html.xpath("//strong/text()")[0]
 
+            print '-----------------------'
+            print name,head
 
-    for i,name in enumerate(names):
-        src = img_srcs[i]
-        players[name] = src
+            time.sleep(10)
 
 def get_words():
     words = set()
     url = "https://hanyu.baidu.com/shici/detail?pid=0b2f26d4c0ddb3ee693fdb1137ee1b0d&from=kg0"
     resp = requests.get(url,headers=HEADERS)
-    for w in resp.content:
+
+    # print resp.content.decode("gb2312")
+    resp.encoding = "utf-8"
+    html = resp.text
+
+    for w in html:
         words.add(w)
     return words
 
 def main():
     words = get_words()
-    print words
     for w in words:
-        print w
+
+
+def test():
+    url = "https://weixin.sogou.com/weixin?query=%E6%9D%8E&_sug_type_=&s_from=input&_sug_=n&type=1&page=2&ie=utf8"
+    resp = requests.get(url,headers=HEADERS)
+    
+    html = etree.HTML(resp.content)
+    # img_srcs = html.xpath("//li//div[@class='img-box']//img/@src")
+    detail_srcs = html.xpath("//li//div/p[@class='tit']/a/@href")
+
+    for src in detail_srcs:
+        resp = requests.get(src,headers=HEADERS)
+        html = etree.HTML(resp.content)
+        head = html.xpath("//div//span/img/@src")[0].replace("http","https")
+        name = html.xpath("//strong/text()")[0]
+
+        sql = "insert into robot(name,head) values(%s,%s)"
+        cursor.execute(sql,(name,head))
+
+        time.sleep(2)
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    test()
